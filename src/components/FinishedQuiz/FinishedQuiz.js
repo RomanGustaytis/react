608697fb469356602a834a93b97cd598b814bdf4
RomanGustaytis <roman.gustaytis@gmail.com>
2019-07-01 17:53:37 +0300

    Вывод результата

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -5,16 +5,32 @@ const FinishedQuiz = props => {
     return (
         <div className={classes.FinishedQuiz}>
             <ul>
-                <li>
-                    <strong>1. </strong>
-                    How are you
-                    <i className={'fa fa-times ' + classes.success} />
-                </li>
-                <li>
-                    <strong>2. </strong>
-                    How are you
-                    <i className={'fa fa-check ' + classes.error} />
-                </li>
+                { props.quiz.map((quizItem, index) => {
+                    const cls = [
+                        'fa',
+                        props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+                        classes[props.results[quizItem.id]]
+                    ]
+                    return (
+                        <li
+                            key={index}
+                        >
+                            <strong>{index + 1}</strong>.&nbsp;
+                            {quizItem.question}
+                            <i className={cls.join(' ')} />
+                        </li>
+                    )
+                }) }
+                {/*<li>*/}
+                {/*    <strong>1. </strong>*/}
+                {/*    How are you*/}
+                {/*    <i className={'fa fa-times ' + classes.success} />*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*    <strong>2. </strong>*/}
+                {/*    How are you*/}
+                {/*    <i className={'fa fa-check ' + classes.error} />*/}
+                {/*</li>*/}
             </ul>
             <p>Правильно 4 из 10</p>
             <div>
